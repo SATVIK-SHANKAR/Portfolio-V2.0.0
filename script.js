604674256c4eef68f2103a05c0e9f115e062cff2
SATVIK-SHANKAR <satvik.shankar2003@gmail.com>
2024-03-19 15:33:21 +0800

    script js addition
    script js addition

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+const fullName = document.getElementById("Name");
+const email = document.getElementById("Email");
+const subject = document.getElementById("Subject");
+const message = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br>Message: ${message.value}`;
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "satvik.shankar2003@gmail.com",
+        Password : "2119A9AAF83703AFD1446B841D3BCC161C22",
+        To : 'satvik.shankar2003@gmail.com',
+        From : "satvik.shankar2003@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => alert(message)
+    );
+}
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    sendEmail();
+})
